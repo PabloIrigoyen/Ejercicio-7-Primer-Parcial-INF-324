f2c0ff4852301730424e9f52915750ed0d722959
PabloIrigoyen <pvillarroeli@fcpn.edu.bo>
2024-05-10 17:40:27 -0400

    Update sumaMonto.php

--- ejercicio7/sumaMonto.php
+++ ejercicio7/sumaMonto.php
@@ -21,114 +21,92 @@
           <th>Monto</th>
         </tr>
       </thead>
-      <tbody>
+      <tbody> 
         <tr>
           <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'La Paz'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >La Paz</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-          <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Cochabamba'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Cochabamba</td>
-            <td ><?php if($total>0){echo $total;}else{echo "0";}?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Santa Cruz'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();
-          $total = $row["total"];
-          
-        ?>
-            <td >Santa Cruz</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Tarija'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Tarija</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Sucre'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Sucre</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Potosi'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-        ?>
-            <td >Potosi</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Oruro'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Oruro</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Beni'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Beni</td>
-            <td ><?php echo $total?>&nbsp&nbsp</td>
-             
-        </tr>
-        <tr>
-        <?php
-          $sql = "SELECT SUM(saldo) AS total FROM cuenta WHERE departamento = 'Pando'";
-          $result = $conexion->query($sql);
-          $row = $result->fetch_assoc();      
-          $total = $row["total"];
-          
-          ?>
-            <td >Pando</td>
-            <td ></td>
-             
+          $sql = "SELECT departamento, SUM(CASE WHEN departamento='La Paz' THEN saldo ELSE 0 END) AS lp,
+                  SUM(CASE WHEN departamento='Cochabamba' THEN saldo ELSE 0 END) AS cbba,
+                  SUM(CASE WHEN departamento='Santa Cruz' THEN saldo ELSE 0 END) AS stc,
+                  SUM(CASE WHEN departamento='Tarija' THEN saldo ELSE 0 END) AS trj,
+                  SUM(CASE WHEN departamento='Sucre' THEN saldo ELSE 0 END) AS suc,
+                  SUM(CASE WHEN departamento='Potosi' THEN saldo ELSE 0 END) AS pts,
+                  SUM(CASE WHEN departamento='Oruro' THEN saldo ELSE 0 END) AS oru,
+                  SUM(CASE WHEN departamento='Beni' THEN saldo ELSE 0 END) AS bn,
+                  SUM(CASE WHEN departamento='Pando' THEN saldo ELSE 0 END)  AS pn FROM cuenta GROUP BY departamento DESC";
+          $result = $conexion->query($sql);   
+          if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {  
+              ?>
+              <tr>
+              <td ><?php echo $row['departamento']?>&nbsp&nbsp</td>
+              <?php if($row['departamento']=='La Paz'){
+                ?>
+                <td ><?php echo $row['lp']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Cochabamba'){
+                ?>
+                <td ><?php echo $row['cbba']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Santa Cruz'){
+                ?>
+                <td ><?php echo $row['stc']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Tarija'){
+                ?>
+                <td ><?php echo $row['trj']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Sucre'){
+                ?>
+                <td ><?php echo $row['suc']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Potosi'){
+                ?>
+                <td ><?php echo $row['pts']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Oruro'){
+                ?>
+                <td ><?php echo $row['oru']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Beni'){
+                ?>
+                <td ><?php echo $row['bn']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              <?php
+              if ($row['departamento']=='Pando'){
+                ?>
+                <td ><?php echo $row['pn']?>&nbsp&nbsp</td>
+              <?php
+              }
+              ?>
+              
+              </tr>
+              <?php        
+            }
+          }
+          ?>            
         </tr>
       </tbody>
     </table>
